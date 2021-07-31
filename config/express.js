4ca13cab96728e4f8752b118aa271a910175c260
PapaGex <cryptoblockchainjunkie@gmail.com>
2021-07-31 15:21:09 -0400

    initialized the npm modules

--- config/express.js
+++ config/express.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser');
 module.exports = (app) => {
     
     //TODO: Setup the view engine
-
+    app.get('/', function (req, res) {
+        res.send('root')
+    })
     //TODO: Setup the body parser
 
     //TODO: Setup the static files
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 // TODO: Require Controllers...
+const express = require('config/express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+
 
 module.exports = (app) => {
     // TODO...
