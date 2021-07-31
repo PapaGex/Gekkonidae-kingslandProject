065182797041e109ae514f0aaedfda3a1e4e6171
PapaGex <cryptoblockchainjunkie@gmail.com>
2021-07-30 21:11:46 -0400

    initial commit

---
+++ config/express.js
@@ -0,0 +1,13 @@
+const express = require('config/express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+module.exports = (app) => {
+    
+    //TODO: Setup the view engine
+
+    //TODO: Setup the body parser
+
+    //TODO: Setup the static files
+
+};
