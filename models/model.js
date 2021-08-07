47abb29d76a5f0675e41b53db12250824685737e
kawekaweau <cryptoblockchainjunkie@gmail.com>
2021-08-07 16:11:00 -0400

    started modelJS

--- models/model.js
+++ models/model.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const handlebars = require('handlebars');
-const app = express();
+const mongoose = require('mongoose');
+const Accesory = require('./Accessory');
 
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://kawekaweau:!Brookesia7$@cluster0.lntxf.mongodb.net/Gekkonidae"?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
-
-app.set('view engine', 'hbs');
-
-
+  const gekkoSchema = new mongoose.GekkoSchema({
+    name: {
+        type: String,
+        required: true
+    },
+    species: {
+        type: String,
+        required: true
+    },
+    ImageUrl: {
+        type: String,
+        required: true
+    },
+// {add boolean for adult or hatchling}
+// {habitat descriptor from radio button}
+// {hatch date or close approximation}
+// {ease of care}
+  })
